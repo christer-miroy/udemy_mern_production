d9c0819c4af143edaa1cc38111cef8ebe678cca2
Christer Miroy <chrstrzm.mry@gmail.com>
2023-01-12 23:08:00 +0800

    test revise navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -38,6 +38,7 @@ const Navbar = () => {
             <FaUserCircle />
             {/* dynamic display of user name */}
             {user?.name}
+            {user?.lastName}
             <FaCaretDown />
           </button>
           {/* logout button */}
